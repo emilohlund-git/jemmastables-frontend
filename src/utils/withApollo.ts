1170bf75b2acb7d523238a755803b28106496fea
emilohlund-git <emil@emilohlund.se>
2021-06-20 17:36:34 +0200

    Updated calendar features, mostly mobile responsiveness tuning.

--- src/utils/withApollo.ts
+++ src/utils/withApollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { withApollo as createWithApollo } from "next-apollo";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://jemmastables-server.herokuapp.com/graphql",
   credentials: "include",
   cache: new InMemoryCache(),
   connectToDevTools: true,
